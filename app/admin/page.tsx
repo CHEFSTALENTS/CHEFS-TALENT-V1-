3e35e353be5fc05568a4c232a66cf4a3cfc63807
CHEFSTALENTS <thomasdelcroix2108@gmail.com>
2025-12-16 19:56:04 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -259,7 +259,7 @@ const quickLists = useMemo(() => {
       tone="stone"
       icon={<IconShield />}
     />
-  </div>
+  
 
   {/* 2 colonnes */}
   <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-6">
