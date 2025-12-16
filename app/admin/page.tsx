32bbcef225fa97df4f66c36b5107ed9a6d0e8e5a
CHEFSTALENTS <thomasdelcroix2108@gmail.com>
2025-12-16 19:42:46 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -266,7 +266,7 @@ const quickLists = useMemo(() => {
     {/* À traiter maintenant */}
     ...
   </div>
-</>
+
           {/* 2 colonnes */}
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-6">
             {/* À traiter maintenant */}
