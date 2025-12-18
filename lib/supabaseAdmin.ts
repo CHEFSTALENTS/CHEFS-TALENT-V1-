969b63d2fd77c3ed079acf578d1fd8877694a9fb
CHEFSTALENTS <thomasdelcroix2108@gmail.com>
2025-12-18 14:19:22 +0100

    Update supabaseAdmin.ts

--- lib/supabaseAdmin.ts
+++ lib/supabaseAdmin.ts
@@ -1,9 +1,14 @@
-// lib/supabaseAdmin.ts
 import { createClient } from '@supabase/supabase-js';
 
-const url = process.env.SUPABASE_URL!;
-const serviceRole = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-export const supabaseAdmin = createClient(url, serviceRole, {
-  auth: { persistSession: false },
-});
+export const supabaseAdmin = createClient(
+  supabaseUrl,
+  serviceRoleKey,
+  {
+    auth: {
+      persistSession: false,
+    },
+  }
+);
