29c2ddc5b7a92d811a9c0f6ee0b7b093109ee0c2
CHEFSTALENTS <thomasdelcroix2108@gmail.com>
2026-01-06 23:09:29 +0100

    Update page.tsx

--- app/chef/dashboard/page.tsx
+++ app/chef/dashboard/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { supabase } from '@/services/supabaseClient';
 import { ChefLayout } from '../../../components/ChefLayout';
