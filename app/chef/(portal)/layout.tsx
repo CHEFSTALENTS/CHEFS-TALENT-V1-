34dff076bad19042a88f1a2c39105e35f381963b
CHEFSTALENTS <thomasdelcroix2108@gmail.com>
2026-01-13 14:51:55 +0100

    Delete app/chef/(portal) directory

--- app/chef/(portal)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ChefLayout } from '@/components/ChefLayout';
-
-export default function ChefPortalLayout({ children }: { children: React.ReactNode }) {
-  return <ChefLayout>{children}</ChefLayout>;
-}
