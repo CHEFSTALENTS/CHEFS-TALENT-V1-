f64c438eb71c3397382a350307b4768953fb39c8
CHEFSTALENTS <thomasdelcroix2108@gmail.com>
2026-01-13 14:44:05 +0100

    Create layout.tsx

---
+++ app/chef/(portal)/layout.tsx
@@ -0,0 +1,5 @@
+import { ChefLayout } from '@/components/ChefLayout';
+
+export default function ChefPortalLayout({ children }: { children: React.ReactNode }) {
+  return <ChefLayout>{children}</ChefLayout>;
+}
